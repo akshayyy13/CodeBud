f03c9bf0a9192ca01fb11e2fa66bfe9af79dfc35
Akshay Shrivastava <94282391+akshayyy13@users.noreply.github.com>
2024-04-03 15:29:19 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -22,7 +22,7 @@ database.connect();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://codebud-frontend-nqkc3iy01-akshays-projects-9be52a00.vercel.app/",
     // Access-Control-Allow-Origin: *,
     credentials:true,
 
